480e657133d75aac38f185a721c3b7b7b9ae92a7
bilalaktas15 <bilal.aktas.15@gmail.com>
2023-03-16 18:43:41 +0100

    first commit

---
+++ run.js
@@ -0,0 +1,9 @@
+
+
+let myLib = require("./index");
+
+console.log(myLib.suma(2, 3))
+console.log(myLib.resta(10, 3))
+console.log(myLib.producto(5, 3))
+console.log(myLib.division(8, 2))
+console.log(cuadrado(5)); // salida: 25
